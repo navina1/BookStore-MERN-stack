b9b68bf42b9f5af32e265443f4d200a3eb15f627
Pranita Turrey <pranitadrk@yahoo.com>
2024-02-17 09:56:29 +0530

    Added author, User, Purchase History

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -6,5 +6,8 @@ db.mongoose=mongoose;
 db.url=dbConfig.url;
 db.books=require("./books.model")(mongoose);
 db.genre=require("./genre.model")(mongoose);
+db.authors=require("./authors.model")(mongoose);
+db.users=require("./users.model")(mongoose);
+db.purchase_history=require("./purchase_history.model")(mongoose);
 
 module.exports = db;
